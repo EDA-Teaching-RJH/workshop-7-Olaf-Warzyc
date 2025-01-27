982adb42c66d70bdf751d980676338d6079a341d
Olaf-Warzyc <148963448+Olaf-Warzyc@users.noreply.github.com>
2025-01-27 15:54:40 +0000

    adding some files not finished

--- client.c
+++ client.c
@@ -10,6 +10,7 @@ int main(int argc, char const *argv[]) {
     struct sockaddr_in serv_addr;
     char *hello = "Hello from client";
     char buffer[1024] = {0};
+    char *pepe = "pepe frog";
     
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
@@ -28,9 +29,16 @@ int main(int argc, char const *argv[]) {
         printf("\nConnection Failed \n");
         return -1;
     }
+
     send(sock, hello, strlen(hello), 0);
     printf("Hello message sent\n");
     valread = read(sock, buffer, 1024);
     printf("%s\n", buffer);
+
+    send(sock, pepe, strlen(pepe), 0);
+    printf("Hello message sent\n");
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+
     return 0;
 }
--- server.c
+++ server.c
@@ -46,5 +46,13 @@ int main() {
     printf("%s\n", buffer);
     send(new_socket, hello, strlen(hello), 0);
     printf("Hello message sent\n");
+
+    memset(buffer, 0, sizeof(buffer));
+
+    recv(new_socket, buffer, 1024,0);
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
     return 0;
 }
